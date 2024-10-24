0257379a14f87366bb434b0d2d480ea003e22fbf
naman-398 <you@example.com>
2024-10-24 12:35:16 +0530

    code merge

--- src/utilits/helper.js
+++ src/utilits/helper.js
@@ -1,4 +1,5 @@
 import review_img from "../assets/images/webp/review-image.webp";
+import { ABOUT_PATH, BLOG_PATH, CONTACT_PATH, HOME_PATH, SERVICES_PATH } from "./Constant";
 
 export const REVIEWS = [
     {
@@ -13,4 +14,11 @@ export const REVIEWS = [
       img: review_img,
       name: 'JOHN HOOD, NY',
     },
+  ];
+  export const NavLinks = [
+    { path: HOME_PATH, title: "Home" },
+    { path: ABOUT_PATH, title: "About" },
+    { path: BLOG_PATH, title: "Blog" },
+    { path: CONTACT_PATH, title: "Contact" },
+    { path: SERVICES_PATH, title: "Services" },
   ];
