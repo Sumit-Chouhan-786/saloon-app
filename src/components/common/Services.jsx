aed25fc123d93c28a066332f1ffd64299d198771
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-10-24 10:29:59 +0530

    add font family

--- src/components/common/Services.jsx
+++ src/components/common/Services.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Services = () => {
   return (
-    <div className="max-w-[1332px] px-4 mx-auto">
+    <div className="lg:max-w-[1332px] container px-4 mx-auto">
       <p>OUR TREATMENT</p>
       <h2>SERVICES</h2>
     </div>
