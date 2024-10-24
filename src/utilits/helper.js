7e523f79eb609b62cbd94a39efdec2cdfe4b5f49
sukhvinder-singh-166 <monudhaka829@gmail.com>
2024-10-24 11:53:11 +0530

    navbar condition add

--- src/utilits/helper.js
+++ src/utilits/helper.js
@@ -1,1 +1,15 @@
-// jkfjkfj
+import {
+  ABOUT_PATH,
+  BLOG_PATH,
+  CONTACT_PATH,
+  HOME_PATH,
+  SERVICES_PATH,
+} from "./Constant";
+// nav links
+export const NavLinks = [
+  { path: HOME_PATH, title: "Home" },
+  { path: ABOUT_PATH, title: "About" },
+  { path: BLOG_PATH, title: "Blog" },
+  { path: CONTACT_PATH, title: "Contact" },
+  { path: SERVICES_PATH, title: "Services" },
+];
