20a722353f0ffae8ea1264df9ddcb64a616a9fba
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-10-24 12:01:23 +0530

    footer start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Services from "../components/common/Services";
 import Nav from "../components/common/Nav";
-
+import Counting from "../components/common/Counting";
+import Discount from "../components/common/Discount";
 const Home = () => {
   return (
     <>
       <div className=" bg-black">
         <Nav />
         <Services />
+        <Counting />
+        <Discount />
       </div>
     </>
   );
--- src/utilits/icons.js
+++ src/utilits/icons.js
@@ -36,3 +36,178 @@ export const NavMenu = () => {
     </svg>
   );
 };
+
+export const RagerIcon = () => {
+  return (
+    <svg
+      width="42"
+      height="74"
+      viewBox="0 0 42 74"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M36.589 1H5.41101C3.20187 1 1.41101 2.79086 1.41101 5V9.498C1.41101 11.7071 3.20187 13.498 5.41101 13.498H36.589C38.7982 13.498 40.589 11.7071 40.589 9.498V5C40.589 2.79086 38.7982 1 36.589 1Z"
+        stroke="#E9C664"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5.81506 7.24902H36.1851"
+        stroke="#E9C664"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.021 13.498C9.01089 14.9808 9.5901 16.4069 10.6312 17.4626C11.6724 18.5184 13.0902 19.1174 14.573 19.128H27.427C28.9097 19.1174 30.3276 18.5184 31.3688 17.4626C32.4099 16.4069 32.9891 14.9808 32.979 13.498V13.498"
+        stroke="#E9C664"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M25.4781 19.125V69.075C25.4066 70.1634 24.9077 71.1794 24.0902 71.9015C23.2728 72.6236 22.203 72.9933 21.1141 72.93C20.0254 72.9931 18.9559 72.6232 18.1386 71.9011C17.3214 71.179 16.8226 70.1632 16.7511 69.075V19.125"
+        stroke="#E9C664"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+
+};
+
+export const ScissorIcon = () => {
+  return (
+    <svg
+      width="50"
+      height="73"
+      viewBox="0 0 50 73"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.0991 54.104L21.5011 38.462C21.5011 38.462 12.5011 17.233 10.0611 11.268C7.62106 5.303 12.6031 1 12.6031 1L25.0891 30.924H25.101L37.588 1.002C37.588 1.002 42.575 5.302 40.132 11.27C37.689 17.238 28.6891 38.462 28.6891 38.462L33.0611 53.994"
+        stroke="#E9C664"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M21.0331 61.83C21.0337 63.9776 20.3111 66.0628 18.9818 67.7496C17.6525 69.4363 15.7939 70.6263 13.7057 71.1277C11.6174 71.629 9.42121 71.4126 7.47101 70.5132C5.5208 69.6139 3.93022 68.084 2.95575 66.1702C1.98127 64.2565 1.67966 62.0703 2.09949 59.9642C2.51931 57.858 3.63616 55.9546 5.26996 54.5607C6.90376 53.1669 8.95929 52.3638 11.1053 52.2809C13.2513 52.1981 15.3627 52.8403 17.0991 54.104"
+        stroke="#E9C664"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M33.0611 53.995C34.8146 52.771 36.9281 52.1707 39.0633 52.29C41.1985 52.4093 43.2318 53.2415 44.8381 54.6532C46.4443 56.065 47.5306 57.9748 47.9229 60.077C48.3153 62.1792 47.991 64.3522 47.0022 66.2484C46.0134 68.1445 44.4171 69.6541 42.4687 70.5356C40.5203 71.4171 38.3326 71.6195 36.2556 71.1104C34.1785 70.6014 32.3323 69.4102 31.0124 67.7277C29.6924 66.0452 28.9751 63.9685 28.9751 61.83"
+        stroke="#E9C664"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M21.033 61.8301C21.095 59.1791 21.613 43.3741 25.007 43.3741C28.401 43.3741 28.915 59.1841 28.974 61.7031"
+        stroke="#E9C664"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+
+};
+
+export const ShopIcon = () => {
+  return (
+    <svg
+      width="62"
+      height="53"
+      viewBox="0 0 62 53"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M29.0666 28.697H12.0366V43.203H29.0666V28.697Z"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M55.4216 23.773V50.681"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M6.65576 23.8081V50.6821"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M59.8466 15.077H2.22559L8.82559 2H53.1586L57.3286 10.152L59.8466 15.077Z"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M45.3925 18.986C45.3925 21.863 42.1655 24.197 38.1865 24.197C34.2075 24.197 30.9805 21.863 30.9805 18.986"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M59.8475 15.077L59.8035 18.992C59.7829 20.0473 59.3672 21.0564 58.6385 21.82C57.7628 22.7501 56.6509 23.4247 55.4215 23.772C54.5047 24.0507 53.5518 24.1926 52.5935 24.193C51.8853 24.194 51.179 24.1196 50.4865 23.971C48.9839 23.6756 47.6087 22.9242 46.5485 21.819C45.8373 21.0795 45.4227 20.1043 45.3835 19.079V15.079L59.8475 15.077Z"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M31.0466 15.075L30.9796 18.988C30.9796 21.867 27.7516 24.188 23.7696 24.188C19.7876 24.188 16.5696 21.863 16.5696 18.988"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2.15259 18.988C2.15259 21.177 4.01559 23.038 6.65259 23.807C7.53122 24.0655 8.44272 24.1952 9.35859 24.192C13.3406 24.192 16.5686 21.867 16.5686 18.992V15.075H2.23059L2.15259 18.988Z"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M49.8056 50.682V28.697H36.7656V50.682"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M59.8466 50.6819H2.22559"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M45.2275 38.673V40.705"
+        stroke="#E9C664"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+
+};
