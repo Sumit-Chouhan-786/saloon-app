1d6faa74f541f9d0013567c1be208d5ca52eefd1
sukhvinder-singh-166 <monudhaka829@gmail.com>
2024-10-24 12:03:43 +0530

    hero section complete with condition

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 import Nav from "./common/Nav";
 import heroLogo from "../assets/images/webp/hero-logo.webp";
+import { useLocation } from "react-router-dom";
+import { ABOUT_PATH, BLOG_PATH, CONTACT_PATH, HOME_PATH, SERVICES_PATH } from "../utilits/Constant";
 const Hero = () => {
+  const useLocationPath = useLocation();
+  const pathname = useLocationPath.pathname;
   return (
     <>
-      <div className="bg-hero flex flex-col bg-cover bg-center bg-no-repeat min-h-screen">
+      <div
+        className={`bg-hero flex flex-col bg-cover bg-center bg-no-repeat ${
+          pathname === HOME_PATH
+            ? "min-h-screen"
+            : " lg:min-h-[400px] min-h-[300px]"
+        }`}
+      >
         <Nav />
         <div className="lg:max-w-[1332px] container px-4 mx-auto lg:py-10 py-5 flex-grow-[1] h-full flex items-center justify-center">
-          <img src={heroLogo} alt="hero" className="w-full max-w-[864px]" />
+          {pathname === HOME_PATH && (
+            <img src={heroLogo} alt="hero" className="w-full max-w-[864px]" />
+          )}
+          {pathname === ABOUT_PATH && (
+            <h1 className="text-white text-center font-normal lg:text-[70px] md:text-[50px] text-[40px] font-abril uppercase">
+              About Us
+            </h1>
+          )}
+          {pathname === BLOG_PATH && (
+            <h1 className="text-white text-center font-normal lg:text-[70px] md:text-[50px] text-[40px] font-abril uppercase">
+              Blog
+            </h1>
+          )}
+          {pathname === CONTACT_PATH && (
+            <h1 className="text-white text-center font-normal lg:text-[70px] md:text-[50px] text-[40px] font-abril uppercase">
+              Contact Us
+            </h1>
+          )}
+          {pathname === SERVICES_PATH && (
+            <h1 className="text-white text-center font-normal lg:text-[70px] md:text-[50px] text-[40px] font-abril uppercase">
+              Services
+            </h1>
+          )}
         </div>
       </div>
     </>
--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -20,7 +20,7 @@ const Nav = () => {
 
   return (
     <>
-      <div className="lg:max-w-[1332px] container px-4 mx-auto md:py-10 py-5 flex-grow-0">
+      <div className="lg:max-w-[1332px] container px-4 mx-auto md:pt-10 pt-5 flex-grow-0">
         <div className="flex justify-between items-center relative z-50">
           <a href="/">
             <NavLogo />
